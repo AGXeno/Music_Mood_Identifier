c1a090c9e1cda5ddc40c7f744b0629c32a687ec5
AGXeno <agblue48@pm.me>
2025-07-04 10:27:15 -0600

    Fix MQTT WebSocket security: use wss:// protocol for HTTPS deployment

--- src/config/mqttConfig.js
+++ src/config/mqttConfig.js
@@ -1,7 +1,7 @@
 const mqttConfig = {
     host: 'broker.hivemq.com',     // HiveMQ's public broker
-    port: 8000,                    // WebSocket port
-    protocol: 'ws',                // Using WebSocket protocol
+    port: 8884,                    // Secure WebSocket port
+    protocol: 'wss',               // Using secure WebSocket protocol
     path: '/mqtt',                 // Required for HiveMQ WebSocket
     clientId: 'moodtunes_' + Math.random().toString(16).substr(2, 8),
     clean: true,
